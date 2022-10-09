cdafb8cbe0357cb3498b74ebc26b2e7919bfe532
markiessuah <90101671+markiessuah@users.noreply.github.com>
2022-10-09 21:12:35 +0800

    Delete homework2 directory

--- homework2/js/vega_lite_vis.js
+++
@@ -1,4 +0,0 @@
-var spec = "https://raw.githubusercontent.com/markiessuah/FIT3179/main/homework2/js/visualization.vl.json";
-vegaEmbed('#vis', spec).then(function(result) {
-  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
