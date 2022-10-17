224fc81fbe79b922fd43410510d8dbe4604e25e2
markiessuah <90101671+markiessuah@users.noreply.github.com>
2022-10-17 19:01:52 +0800

    Delete assignment2 directory

--- assignment2/js/v6.js
+++
@@ -1,4 +0,0 @@
-var spec = "https://raw.githubusercontent.com/markiessuah/FIT3179/main/assignment2/js/others.json";
-vegaEmbed('#vis6', spec).then(function(result) {
-  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
--- assignment2/js/v8.js
+++
@@ -1,4 +0,0 @@
-var spec = "https://raw.githubusercontent.com/markiessuah/FIT3179/main/assignment2/js/sega.json";
-vegaEmbed('#vis8', spec).then(function(result) {
-  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
